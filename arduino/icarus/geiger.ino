6dbe43285d0d21fa489fd5c96ba11ff5332db617
pitiplin <c.g.ambrozaitis@hotmail.com>
2015-08-03 14:25:43 +0200

    LED bar removed for optimization purpose

--- arduino/icarus/geiger.ino
+++ arduino/icarus/geiger.ino
@@ -1,87 +1,44 @@
-/*  
- *  Geiger Counter - Radiation Sensor Board
- *  
- *  Copyright (C) Libelium Comunicaciones Distribuidas S.L. 
- *  http://www.libelium.com 
- *  
- *  This program is free software: you can redistribute it and/or modify 
- *  it under the terms of the GNU General Public License as published by 
- *  the Free Software Foundation, either version 3 of the License, or 
- *  (at your option) any later version. 
- *  a
- *  This program is distributed in the hope that it will be useful, 
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of 
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
- *  GNU General Public License for more details.
- *  
- *  You should have received a copy of the GNU General Public License 
- *  along with this program.  If not, see http://www.gnu.org/licenses/. 
- *  
- *  Version:           3.0
- *  Design:            David Gascón 
- *  Implementation:    Marcos Yarza
- */
-
-// Threshold values for the led bar
-#define TH1 45
-#define TH2 95
-#define TH3 200
-#define TH4 400
-#define TH5 600
-
-// Conversion factor - CPM to uSV/h
-#define CONV_FACTOR 0.00812
-
 // Variables
-int ledArray [] = {10,11,12,13,9};
 int geiger_input = 2;
 long count = 0;
 long countPerMinute = 0;
-long timePrevious = 0;
 long timePreviousMeassure = 0;
-long time = 0;
-long countPrevious = 0;
-float radiationValue = 0.0;
 
 void setup(){
   pinMode(geiger_input, INPUT);
   digitalWrite(geiger_input,HIGH);
-  for (int i=0;i<5;i++){
-    pinMode(ledArray[i],OUTPUT);
-  }
-
+  
   Serial.begin(9600);
 
   attachInterrupt(0,countPulse,FALLING);
-
 }
 
 void loop(){
   if (millis()-timePreviousMeassure > 10000){
     countPerMinute = 6*count;
-    radiationValue = countPerMinute * CONV_FACTOR;
     timePreviousMeassure = millis();
     
+    //La variable "countPerMinute" tendrá que ser leída por las Rasberry y almacenada
+    //en un archivo de texto junto con la fecha y hora de la medida
+    //El formato correcto será de la forma "dd/mm/yyyy hh:mm:ss;countPerMinute"
+    //Ejemplo:
+    //  24/07/2015 14:35:14;24
+    //  24/07/2015 14:35:24;24
+    //  24/07/2015 14:35:34;12
+    //  24/07/2015 14:35:44;0
+    //  24/07/2015 14:35:54;12
+    
+    //Esta parte se ha usado para la calibración del instrumento en CaLMa
+    //Será borrada cuando se compile en el sensor
+    //Begin -> GobetWino
     char buffer[20];
     Serial.print("#S|LOGTEST|[");
     Serial.print(itoa((countPerMinute), buffer, 10));
-    Serial.print(";");
-    dtostrf(radiationValue, 6, 5, buffer);  //6 is mininum width, 6 is precision; float value is copied onto buff
-    Serial.print(String(buffer)); //display string    
     Serial.println("]#");
-    
-    //led var setting  
-    if(countPerMinute <= TH1) ledVar(0);
-    if((countPerMinute <= TH2)&&(countPerMinute>TH1)) ledVar(1);
-    if((countPerMinute <= TH3)&&(countPerMinute>TH2)) ledVar(2);
-    if((countPerMinute <= TH4)&&(countPerMinute>TH3)) ledVar(3);
-    if((countPerMinute <= TH5)&&(countPerMinute>TH4)) ledVar(4);
-    if(countPerMinute>TH5) ledVar(5);
-
+    //End -> GobetWino
+   
     count = 0;
-
   }
-
 }
 
 void countPulse(){
@@ -91,20 +48,3 @@ void countPulse(){
   }
   attachInterrupt(0,countPulse,FALLING);
 }
-
-void ledVar(int value){
-  if (value > 0){
-    for(int i=0;i<=value;i++){
-      digitalWrite(ledArray[i],HIGH);
-    }
-    for(int i=5;i>value;i--){
-      digitalWrite(ledArray[i],LOW);
-    }
-  }
-  else {
-    for(int i=5;i>=0;i--){
-      digitalWrite(ledArray[i],LOW);
-    }
-  }
-}
-    

93ebdb42f5bb7ff2f83409399b3925ff575fa428
moraprj <moraprj@gmail.com>
2015-08-23 19:00:36 +0200

    Added Raspberry internal (CPU) temp
    Scientific data now contains internal CPU temperature in order to
    compare with external sensor temperature

--- python/save_science_data.py
+++ python/save_science_data.py
@@ -6,9 +6,18 @@ import os
 import shutil
 import threading
 
-def add_data(file, data):
+def get_cpu_temp():
+	f = open("/sys/class/thermal/thermal_zone0/temp")
+	cpu_temp = f.read()
+	f.close()
+	return float(cpu_temp)/1000
+
+def add_data(file, data, add_temp):
     f = open('datos/' + file, 'a')
-    f.write(datetime.datetime.now().isoformat() + ',' + data)
+    data_text = datetime.datetime.now().isoformat() + ',' + data.rstrip()
+    if add_temp == True:
+        data_text = data_text + ',' + str(get_cpu_temp()) + '\n'
+    f.write( data_text + '\n')
     f.close()
     
 def compress_file(file):
@@ -29,28 +38,23 @@ def read_data(port):
     ser.flush()
     while 1:
         new_data = ser.readline()
-        print(new_data)
         if new_data[0] == 'D':
-            add_data(file_science, new_data)
+            add_data(file_science, new_data, add_temp = True)
             num_science_entries += 1
         elif new_data[0] == 'G':
-            add_data(file_geiger, new_data)
+            add_data(file_geiger, new_data, add_temp = False)
             num_geiger_entries += 1
         
         if num_geiger_entries == 100:
-            move_file(file_geiger) 
-    	    #file_geiger = datetime.datetime.now().isoformat() + '-geiger.txt'
+            move_file(file_geiger)
     	    file_geiger = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S-%f") + '-geiger.txt'
     	    num_geiger_entries = 0
         
         if num_science_entries == 5000:
             print(file_science)
             move_file(file_science)
-    	    #file_science = datetime.datetime.now().isoformat() + '-science.txt'
     	    file_science = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S-%f") + '-science.txt'
     	    num_science_entries = 0
     	    
 
-read_data('/dev/cu.usbmodem1411')
-#move_file('datos/2015-08-17T19/14/05.963023-science.txt')
-    
+read_data('/dev/ttyACM0')
